27c58d10f1e291b5b8d88f6d130a190e6400434d
djsy01 <dlsgh8884@naver.com>
2026-02-04 09:48:31 +0900

    컴포넌트 나누기

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-import "./style.css"
+import './style.css';
+
+const sections = ['header', 'banner', 'about', 'download', 'qna', 'apply', 'footer'];
+
+async function loadSection(name) {
+  const el = document.getElementById(name);
+  const res = await fetch(`/sections/${name}.html`);
+  el.innerHTML = await res.text();
+}
+
+sections.forEach(loadSection);
